a07bf661d43f1631b0820941c9fa16a3b419a955
Kevin Kohls <kevinkohls1959@gmail.com>
2018-07-30 22:02:46 -0400

    removed script from index.html performed by install

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import React, { Component } from 'react';
 import './App.css';
 import * as firebase from 'firebase';
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDx4OOWjNLsnDxkWrUbKZgYITBKcrWvAT8",
+    authDomain: "bloc-chat-125e6.firebaseapp.com",
+    databaseURL: "https://bloc-chat-125e6.firebaseio.com",
+    projectId: "bloc-chat-125e6",
+    storageBucket: "bloc-chat-125e6.appspot.com",
+    messagingSenderId: "1073569690929"
+  };
+  firebase.initializeApp(config);
+
 class App extends Component {
   render() {
     return (
